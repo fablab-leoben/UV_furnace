b287b57e25b7d46f2f26129a5e0bd30d448f918b
Thomas <rockenbauer.thomas@gmail.com>
2016-07-23 23:08:23 +0200

    sync utc time via ntp protocol

--- UV_furnace.ino
+++ UV_furnace.ino
@@ -2,6 +2,8 @@
 
 #include <Ethernet2.h>
 
+#include <EthernetUdp2.h>
+
 #include "access.h"
 
 #include <PID_AutoTune_v0.h>
@@ -102,6 +104,22 @@ byte LED2_intens = 0;
 byte LED3_intens = 0;
 
 /************************************************
+Ethernet
+************************************************/
+
+unsigned int localPort = 8888;       // local port to listen for UDP packets
+
+char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+
+/************************************************
  Temperature variables
 ************************************************/
 bool preheat = false;
@@ -147,20 +165,6 @@ unsigned int aTuneLookBack=20;
 boolean tuning = false;
  
 PID_ATune aTune(&Input, &Output);
-
-/************************************************
- NTP
-************************************************/
-
-// NTP Servers:
-IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 103); // time-c.timefreq.bldrdoc.gov
-
-
-const int timeZone = 1;     // Central European Time
-
-unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 /************************************************
  Timer Variables
@@ -1532,11 +1536,13 @@ void setup() {
   selETH();
   if (Ethernet.begin(mac) == 0) {
     // no point in carrying on, so do nothing forevermore:
-    //while (1) {
+    while (1) {
       DEBUG_PRINTLN("Failed to configure Ethernet using DHCP");
-      //delay(10000);
-   // }
+      delay(10000);
+    }
   }
+  Udp.begin(localPort);
+
   DEBUG_PRINTLN("IP number assigned by DHCP is ");
   DEBUG_PRINTLN(Ethernet.localIP());
 
@@ -1769,7 +1775,7 @@ int updateBlynk(){
    if (BlynkInterval < BLYNK_INTERVAL) {
     return 0;
    }
-   DEBUG_PRINTLN(F("updating Blynk"));
+   //DEBUG_PRINTLN(F("updating Blynk"));
    selETH();
    Blynk.virtualWrite(V0, currentTemperature);
 }
@@ -1932,7 +1938,72 @@ void initEnterFunction(){
   
   //start the timer of this cycle
   initTimer = 0;
+
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+  selETH();
+  // wait to see if a reply is available
+  delay(1000);
+  if ( Udp.parsePacket() ) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+
+    Serial.println(RTC.set(epoch));
+
+    setSyncProvider(RTC.get);   // the function to get the time from the RTC
+
+    tmElements_t tm;
+    RTC.read(tm);
+    Serial.print(tm.Day, DEC);
+    Serial.print('.');
+    Serial.print(tm.Month, DEC);
+    Serial.print('.');
+    Serial.print(year(), DEC);
+    Serial.print(' ');
+    Serial.print(tm.Hour, DEC);
+    Serial.print(':');
+    Serial.print(tm.Minute,DEC);
+    Serial.print(':');
+    Serial.println(tm.Second,DEC);
+  }
 }
+
 void initUpdateFunction(){
   //DEBUG_PRINTLN("initUpdate");
   //time is up?
@@ -2188,3 +2259,26 @@ void updateGraph(){
   GraphUpdateInterval = 0;
 }
 
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(char* address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
