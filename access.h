872ac9c55e50d4f1d35233fa5deb22c65678a72d
Thomas <hl68fx@users.noreply.github.com>
2016-09-01 00:53:54 +0200

    updates

--- access.h
+++ access.h
@@ -1,6 +1,9 @@
+//Ethernet shield MAC address
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
+//Blynk authentication code
 char auth[] = "d69a7b2860cc42d29db123073f774715";
 
+//InfluxDB configuration
 char INFLUXDB_HOST[] = "yourdynddns.com";
 int INFLUXDB_PORT = 4444;
