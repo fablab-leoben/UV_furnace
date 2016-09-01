7500e02ce8b1f67a172437fdc770bee810917fa8
Thomas <rockenbauer.thomas@gmail.com>
2016-09-01 06:14:34 +0200

    InfluxDB logging

--- access.h
+++ access.h
@@ -2,8 +2,8 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 //Blynk authentication code
-char auth[] = "d69a7b2860cc42d29db123073f774715";
+char auth[] = "d69a7b2860cc42d29db123073f774714";
 
 //InfluxDB configuration
-char INFLUXDB_HOST[] = "yourdynddns.com";
+char INFLUXDB_HOST[] = "datenhamster.ddns.net";
 int INFLUXDB_PORT = 4444;
