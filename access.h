26da7dafcabef39611f509a7586ad7b5d66f69bf
Thomas <rockenbauer.thomas@gmail.com>
2016-09-01 06:54:34 +0200

    update

--- access.h
+++ access.h
@@ -5,5 +5,5 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char auth[] = "d69a7b2860cc42d29db123073f774715";
 
 //InfluxDB configuration
-char INFLUXDB_HOST[] = "yourdynddns.com";
+char INFLUXDB_HOST[] = "yourdyndns";
 int INFLUXDB_PORT = 4444;
