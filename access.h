e436c031bd0f9b773e777bc06c1977102e4ac968
Thomas <rockenbauer.thomas@gmail.com>
2016-12-14 01:54:08 +0100

    display update

--- access.h
+++ access.h
@@ -1,7 +1,6 @@
 //Blynk authentication code
-char auth[] = "d69a7b2860cc42d29db123073f774715";
+char auth[] = "d69a7b2860cc42d29db123073f774714";
 
 //InfluxDB configuration
 char INFLUXDB_HOST[] = "yourdyndns"; //or your server ip
 int INFLUXDB_PORT = 4444;
-
